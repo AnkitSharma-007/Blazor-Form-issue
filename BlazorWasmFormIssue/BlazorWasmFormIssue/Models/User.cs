929e8c07640e548078306405c89ac6c2ef13f1f4
ankit.sharma <ankit.sharma@extern-hmmh.de>
2021-02-10 21:16:28 +0530

    first commit

---
+++ BlazorWasmFormIssue/BlazorWasmFormIssue/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCourseDemo.Models
+{
+    public class User
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = " The Name must have atlest three characters.")]
+        public string Name { get; set; }
+        [Required]
+        [Range(18, 100, ErrorMessage = " The age must be between 18-100 yrs.")]
+        public int Age { get; set; }
+        [Required]
+        [UserNameValidation]
+        public string UserName { get; set; }
+    }
+}
---
+++ BlazorWasmFormIssue/BlazorWasmFormIssue/Pages/FormDemo.razor.cs
@@ -0,0 +1,48 @@
+﻿using BlazorCourseDemo.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Threading.Tasks;
+
+namespace BlazorWasmFormIssue.Pages
+{
+    public class FormDemoBase : ComponentBase
+    {
+        protected User editContextuser = new();
+        protected EditContext editFormContext;
+        protected bool isFormInvalid = true;
+        protected string showValidationSummary = "display:none";
+
+
+        protected override Task OnInitializedAsync()
+        {
+            editFormContext = new EditContext(editContextuser);
+            editFormContext.OnFieldChanged += HandleFormFieldChangedEvent;
+            return base.OnInitializedAsync();
+        }
+
+        protected void HandleFormFieldChangedEvent(object sender, FieldChangedEventArgs e)
+        {
+            isFormInvalid = !editFormContext.Validate();
+            StateHasChanged();
+        }
+
+        protected void HandleEditContextFormSubmit()
+        {
+            var isFormValid = editFormContext.Validate();
+
+            if (isFormValid)
+            {
+                showValidationSummary = "display:block";
+            }
+            else
+            {
+                showValidationSummary = "display:none";
+            }
+        }
+
+        public void Dispose()
+        {
+            editFormContext.OnFieldChanged -= HandleFormFieldChangedEvent;
+        }
+    }
+}
